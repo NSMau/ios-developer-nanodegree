261f144ac13a9f8181c882392436ec8d48ded1cf
silentnil <mauriciopaternina@icloud.com>
2023-09-12 00:09:04 -0500

    refactor code from click counter example

--- UIKit Fundamentals/Click Counter/Click Counter/ViewController.swift
+++ UIKit Fundamentals/Click Counter/Click Counter/ViewController.swift
@@ -2,103 +2,83 @@
 //  ViewController.swift
 //  Click Counter
 //
-//  Created by Mauricio Paternina on 18/08/22.
+//  Created by Mauricio Paternina on 08/18/22.
+//  Refactored by Mauricio Paternina on 09/11/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
   
-  var count = 0
-  var label = UILabel()
+  private var count = 0
+  private let label = UILabel()
+  private var buttons = UIStackView()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let label = UILabel()
-    let buttonIncrement = UIButton()
-    let buttonDecrement = UIButton()
-    let buttonReset = UIButton()
-    let buttons = UIStackView()
-    let uiStack = UIStackView()
+    configureLabel()
+    configureButtons()
+    configureUI()
+  }
+  
+  private func configureLabel() {
+    label.text = "\(count)"
+    label.font = .systemFont(ofSize: 48)
+    label.translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  private func configureButtons() {
+    let buttonIncrement = makeButton(withTitle: "Increment")
+    let buttonDecrement = makeButton(withTitle: "Decrement")
+    let buttonReset = makeButton(withTitle: "Reset")
     
-    self.label = label
-    
-    label.frame.size = CGSize(width: 64, height: 48)
-    buttonIncrement.frame.size = CGSize(width: 144, height: 48)
-    buttonDecrement.frame.size = CGSize(width: 144, height: 48)
-    buttonReset.frame.size = CGSize(width: 144, height: 48)
-    
-    label.text = "0"
-    label.font = UIFont.systemFont(ofSize: 24)
-    
-    buttonIncrement.setTitle("Increment", for: .normal)
-    buttonDecrement.setTitle("Decrement", for: .normal)
-    buttonReset.setTitle("Reset", for: .normal)
-    
-    buttonIncrement.backgroundColor = UIColor.blue
-    buttonDecrement.backgroundColor = UIColor.blue
-    buttonReset.backgroundColor = UIColor.blue
-    
-    buttonIncrement.setTitleColor(UIColor.white, for: .normal)
-    buttonDecrement.setTitleColor(UIColor.white, for: .normal)
-    buttonReset.setTitleColor(UIColor.white, for: .normal)
-    
-    // Set up stack view for the buttons and ui stacks
-    buttons.axis = NSLayoutConstraint.Axis.vertical
-    uiStack.axis = NSLayoutConstraint.Axis.vertical
-    
-    buttons.distribution = UIStackView.Distribution.equalSpacing
-    uiStack.distribution = UIStackView.Distribution.equalSpacing
-    
-    buttons.alignment = UIStackView.Alignment.fill
-    uiStack.alignment = UIStackView.Alignment.center
-    
-    buttons.spacing = 16.0
-    uiStack.spacing = 24.0
-    
-    // Add buttons to the buttons stack view
     buttons.addArrangedSubview(buttonIncrement)
     buttons.addArrangedSubview(buttonDecrement)
     buttons.addArrangedSubview(buttonReset)
     
-    // Add UI elements to the uiStack view
-    uiStack.addArrangedSubview(label)
-    uiStack.addArrangedSubview(buttons)
-    
-    // AutoLayout Constraints
-    buttons.translatesAutoresizingMaskIntoConstraints = false
-    uiStack.translatesAutoresizingMaskIntoConstraints = false
-    
-    // Add UI elements to the view
-    view.addSubview(uiStack)
-    
-    // Constraints for uiStack
-    uiStack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-    uiStack.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-    
-    // Set constant width for buttons stack
     NSLayoutConstraint.activate([buttons.widthAnchor.constraint(equalToConstant: 160)])
     
     // Link actions to buttons
-    buttonIncrement.addTarget(
-      self,
-      action: #selector(ViewController.incrementCounter),
-      for: UIControl.Event.touchUpInside
-    )
+    buttonIncrement.addTarget(self, action: #selector(incrementCounter), for: .touchUpInside)
+    buttonDecrement.addTarget(self, action: #selector(decrementCounter), for: .touchUpInside)
+    buttonReset.addTarget(self, action: #selector(resetCounter), for: .touchUpInside)
     
-    buttonDecrement.addTarget(
-      self,
-      action: #selector(ViewController.decrementCounter),
-      for: UIControl.Event.touchUpInside
-    )
+    // Set buttonsStackView's UI-related properties
+    buttons.axis = .vertical
+    buttons.distribution = .equalSpacing
+    buttons.alignment = .fill
+    buttons.spacing = 16.0
+    buttons.translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  private func makeButton(withTitle title: String) -> UIButton {
+    let button = UIButton()
     
-    buttonReset.addTarget(
-      self,
-      action: #selector(ViewController.resetCounter),
-      for: UIControl.Event.touchUpInside
-    )
+    button.setTitle(title, for: .normal)
+    button.setTitleColor(.white, for: .normal)
+    button.backgroundColor = .blue
+    button.frame.size = CGSize(width: 144, height: 64)
+    button.translatesAutoresizingMaskIntoConstraints = false
     
+    return button
+  }
+  
+  private func configureUI() {
+    let ui = UIStackView(arrangedSubviews: [label, buttons])
+    
+    ui.axis = .vertical
+    ui.distribution = .equalSpacing
+    ui.alignment = .center
+    ui.spacing = 24.0
+    ui.translatesAutoresizingMaskIntoConstraints = false
+    
+    view.addSubview(ui)
+    
+    NSLayoutConstraint.activate([
+      ui.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      ui.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -24)
+    ])
   }
 
   @objc func incrementCounter() {
